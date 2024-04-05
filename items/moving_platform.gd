75e48c625295cc48c1ef160528c119e1c5a27078
TotallyNotAlex20 <eBSvwX8xBmmx9k2V@gmail.com>
2024-04-05 16:12:28 +0200

    added some items

---
+++ items/moving_platform.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var speed_x = 1
+var speed_y = 0
+var range = Vector2(100, 0)
+var direction = true # true positive false negative
+var start_position
+
+func _ready():
+	start_position = self.position
+
+func _physics_process(delta):
+	if direction:
+		self.position += Vector2(speed_x, speed_y)
+	elif !direction:
+		self.position -= Vector2(speed_x, speed_y)
+	print(start_position + range, abs(self.position))
+	if direction and start_position + range < abs(self.position) or !direction and start_position - range > abs(self.position):
+		direction = !direction
--- odi.gd
+++ odi.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 @onready var animator = get_node("AnimationPlayer")
-
+@onready var x_scale = get_node("body").scale.x
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -600.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -21,6 +21,10 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 		animator.play("walk")
+		if direction < 0:
+			get_node("body").scale.x = x_scale
+		elif direction > 0:
+			get_node("body").scale.x = -x_scale
 	else:
 		animator.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
