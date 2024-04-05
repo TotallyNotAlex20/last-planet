ad76ae03989a99911d8c5bf7db113635f8463a9d
TotallyNotAlex20 <eBSvwX8xBmmx9k2V@gmail.com>
2024-04-05 14:46:50 +0200

    fixed walking animation

--- odi.gd
+++ odi.gd
@@ -5,14 +5,12 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	animator.play("idle")
 
 func _physics_process(delta):
-	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
@@ -22,7 +20,9 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animator.play("walk")
 	else:
+		animator.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
