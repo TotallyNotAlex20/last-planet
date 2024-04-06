8ca5b7a5a5046a129da1a17be7130cd500a43c9a
TotallyNotAlex20 <eBSvwX8xBmmx9k2V@gmail.com>
2024-04-06 17:43:59 +0200

    esc to exit monitor

--- objects/monitor.gd
+++ objects/monitor.gd
@@ -4,5 +4,9 @@ func _ready():
 	ui = get_node("../PCMonitor")
 	ui.visible = false
 
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("exit_gui") and ui.visible:
+		ui.visible = false
+
 func open_monitor():
 	ui.visible = !ui.visible
