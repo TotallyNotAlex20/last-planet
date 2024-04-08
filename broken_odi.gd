3ac062d3a3ead0e2ee0f4693aa697bbbdf4fee10
TotallyNotAlex20 <eBSvwX8xBmmx9k2V@gmail.com>
2024-04-08 13:07:19 +0200

    interact animation for broken obi

--- broken_odi.gd
+++ broken_odi.gd
@@ -1,4 +1,17 @@
 extends CharacterBody2D
-	
+
+@onready var x_scale = $body.scale.x
+
 func set_up(given_position):
 	$body/Fist.position = given_position
+	
+func interact(interact_obj_position):
+	if interact_obj_position.x < self.position.x:
+		$body.scale.x = x_scale
+	elif interact_obj_position.x > self.position.x:
+		$body.scale.x = -x_scale
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("interact"):
+		interact(get_viewport().get_mouse_position())
+		$AnimationPlayer.play("interact")
