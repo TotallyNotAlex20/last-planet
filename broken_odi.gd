943a08fb89aae5f2a4c92044b1b4b96eed6ab119
TotallyNotAlex20 <eBSvwX8xBmmx9k2V@gmail.com>
2024-04-13 13:31:11 +0200

    added an item pickup key

--- broken_odi.gd
+++ broken_odi.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @onready var x_scale = $body.scale.x
 var interactable_body
+var items = [] # items currently in range
 
 func set_up(given_position):
 	$body/Fist.position = given_position
@@ -17,11 +18,22 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("interact") and interactable_body != null:
 		interact(interactable_body.global_position)
 		get_node("../" + interactable_body.name).interact()
+	if Input.is_action_just_pressed("pick_up") and items != null:
+		for item in items:
+			interact(item.global_position)
+			item.free()
+			items.remove_at(items.find(item))
 		
 func _on_area_2d_body_entered(body):
 	if body.has_meta("interactable"):
 		interactable_body = body
+		
+	if body.has_meta("item"): # if item enters detection, add to array
+		items.append(body)
 
 func _on_area_2d_body_exited(body):
 	if body.has_meta("interactable"):
 		interactable_body = null
+		
+	if body.has_meta("item"): # if item exits detection, remove from array
+		items.remove_at(items.find(body))
